f2dca5c2de296103dcaed2f4cf8aa1752698c2a4
asetic0001 <asetic0001@gmail.com>
2023-10-31 08:17:01 +0530

    landing page
    need to add rest of the page

---
+++ script.js
@@ -0,0 +1,20 @@
+function createStar() {
+    const star = document.createElement('div');
+    star.className = 'star';
+    star.style.left = `${Math.random() * 100}vw`;
+    star.style.top = `${Math.random() * 100}vh`;
+    document.getElementById('stars-container').appendChild(star);
+}
+
+setInterval(createStar, 200); // Create twinkling stars every 200ms
+
+// Show "title" and "about" after scrolling down
+window.addEventListener("scroll", () => {
+    const title = document.querySelector(".title");
+    const about = document.querySelector(".about");
+    
+    if (window.scrollY > 100) {
+        title.classList.remove("hidden");
+        about.classList.remove("hidden");
+    }
+});
